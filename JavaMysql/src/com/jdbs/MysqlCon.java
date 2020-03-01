708b929b95d822bb39e3f5205ac723abafd93b0b
vipul2996 <vipul.depolkar96@gmail.com>
2020-02-29 23:17:48 -0800

    added new projets

---
+++ JavaMysql/src/com/jdbs/MysqlCon.java
@@ -0,0 +1,13 @@
+package com.jdbs;
+public class MysqlCon {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ JavaMysql/src/com/jdbs/Pattern.java
@@ -0,0 +1,28 @@
+package com.jdbs;
+class Pattern
+{
+	
+public static void main(String args[] )
+{
+	int n=5;
+	for(int i=1;i<n;i++)
+	{
+		for(int j=n-1;j>=i;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int k=0;k<=i;k++)
+		{
+			System.out.print("*");
+		}
+		
+		System.out.println();
+		
+	}
+
+	
+	
+	
+	
+}
+}
