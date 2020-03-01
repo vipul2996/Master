d6f480f23117b1fda646d802f524e86efbea1dde
vipul2996 <vipul.depolkar96@gmail.com>
2020-02-29 23:46:05 -0800

    added

--- JavaMysql/src/com/jdbs/MysqlCon.java
+++ JavaMysql/src/com/jdbs/MysqlCon.java
@@ -3,11 +3,7 @@ public class MysqlCon {
 
 	public static void main(String[] args) {
 		
-		System.out.print("blakns");
-		System.out.print("blakns");
-		System.out.print("blakns");
-		System.out.print("blakns");
-		System.out.print("blakns");
+		
 		
 		
 		
